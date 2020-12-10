4a3801692524e513dd70796144ba7f4d4f3539dd
Joe Attueyi <jattueyi@gmail.com>
2020-12-10 15:04:27 -0800

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Joe Attueyi on 12/10/20.
+//  Copyright © 2020 Joe Attueyi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
